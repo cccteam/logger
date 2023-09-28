50a862d22546fe563ec8f39a053e31e6fa766bc6
Zach Redinger <zach@cloud-team.com>
2023-09-28 19:23:48 +0000

    add aws logger and upgrade deps

---
+++ aws.go
@@ -0,0 +1,150 @@
+package logger
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"sync"
+
+	"go.opentelemetry.io/otel/trace"
+)
+
+type AWSExporter struct {
+	logAll bool
+}
+
+func NewAWSExporter() *AWSExporter {
+	return &AWSExporter{}
+}
+
+func (e *AWSExporter) Middleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return &awsHandler{
+			next:         next,
+			parentLogger: slog.New(slog.NewJSONHandler(os.Stdout, nil)).WithGroup("request_parent_log"),
+			childLogger:  slog.New(slog.NewJSONHandler(os.Stdout, nil)).WithGroup("request_child_log"),
+			logAll:       e.logAll,
+		}
+	}
+}
+
+type awsHandler struct {
+	next         http.Handler
+	parentLogger *slog.Logger
+	childLogger  *slog.Logger
+	logAll       bool
+}
+
+func (h *awsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	traceID := r.Header.Get("X-Amzn-Trace-Id")
+	l := newAWSLogger(h.childLogger, traceID)
+	r = r.WithContext(newContext(r.Context(), l))
+	sw := &statusWriter{ResponseWriter: w}
+
+	h.next.ServeHTTP(sw, r)
+
+	l.mu.Lock()
+	logCount := l.logCount
+	maxLevel := l.maxLevel
+	l.mu.Unlock()
+
+	if !h.logAll && logCount == 0 {
+		return
+	}
+
+	if sw.Status() > 399 && maxLevel < slog.LevelError {
+		maxLevel = slog.LevelError
+	}
+
+	sc := trace.SpanContextFromContext(r.Context())
+	logAttr := []slog.Attr{
+		slog.Any("trace", sc.TraceID().String()),
+		slog.Any("spanID", sc.SpanID().String()),
+	}
+	logAttr = append(logAttr, httpRequestAttributes(r)...)
+	h.parentLogger.LogAttrs(r.Context(), maxLevel, "Parent Log Entry", logAttr...)
+}
+
+type awsLogger struct {
+	logger   *slog.Logger
+	traceID  string
+	mu       sync.Mutex
+	maxLevel slog.Level
+	logCount int
+}
+
+func newAWSLogger(logger *slog.Logger, traceID string) *awsLogger {
+	return &awsLogger{
+		logger:  logger,
+		traceID: traceID,
+	}
+}
+
+// Debug logs a debug message.
+func (l *awsLogger) Debug(ctx context.Context, v interface{}) {
+	l.log(ctx, slog.LevelDebug, fmt.Sprint(v))
+}
+
+// Debugf logs a debug message with format.
+func (l *awsLogger) Debugf(ctx context.Context, format string, v ...interface{}) {
+	l.log(ctx, slog.LevelDebug, fmt.Sprintf(format, v...))
+}
+
+// Info logs a info message.
+func (l *awsLogger) Info(ctx context.Context, v interface{}) {
+	l.log(ctx, slog.LevelInfo, fmt.Sprint(v))
+}
+
+// Infof logs a info message with format.
+func (l *awsLogger) Infof(ctx context.Context, format string, v ...interface{}) {
+	l.log(ctx, slog.LevelInfo, fmt.Sprintf(format, v...))
+}
+
+// Warn logs a warning message.
+func (l *awsLogger) Warn(ctx context.Context, v interface{}) {
+	l.log(ctx, slog.LevelWarn, fmt.Sprint(v))
+}
+
+// Warnf logs a warning message with format.
+func (l *awsLogger) Warnf(ctx context.Context, format string, v ...interface{}) {
+	l.log(ctx, slog.LevelWarn, fmt.Sprintf(format, v...))
+}
+
+// Error logs an error message.
+func (l *awsLogger) Error(ctx context.Context, v interface{}) {
+	l.log(ctx, slog.LevelError, fmt.Sprint(v))
+}
+
+// Errorf logs an error message with format.
+func (l *awsLogger) Errorf(ctx context.Context, format string, v ...interface{}) {
+	l.log(ctx, slog.LevelError, fmt.Sprintf(format, v...))
+}
+
+func (l *awsLogger) log(ctx context.Context, level slog.Level, message string) {
+	l.mu.Lock()
+	if l.maxLevel < level {
+		l.maxLevel = level
+	}
+	l.logCount++
+	l.mu.Unlock()
+
+	span := trace.SpanFromContext(ctx)
+
+	l.logger.Log(ctx, level, message, slog.Any("spanID", span.SpanContext().SpanID().String()))
+}
+
+func httpRequestAttributes(r *http.Request) []slog.Attr {
+	return []slog.Attr{
+		slog.String("http.method", r.Method),
+		slog.String("http.host", r.Host),
+		slog.String("http.target", r.URL.Path),
+		slog.String("http.user_agent", r.UserAgent()),
+		slog.String("http.scheme", r.URL.Scheme),
+		slog.String("http.flavor", r.Proto),
+		slog.String("http.client_ip", r.RemoteAddr),
+		slog.Int64("http.request_content_length", r.ContentLength),
+		slog.String("http.request_id", r.Header.Get("X-Request-Id")),
+	}
+}
--- go.mod
+++ go.mod
@@ -3,17 +3,17 @@ module github.com/jtwatson/logger
 go 1.21
 
 require (
-	cloud.google.com/go/logging v1.7.0
+	cloud.google.com/go/logging v1.8.1
 	contrib.go.opencensus.io/exporter/stackdriver v0.13.14
-	github.com/go-playground/errors/v5 v5.3.0
+	github.com/go-playground/errors/v5 v5.3.3
 	github.com/go-test/deep v1.1.0
-	go.opentelemetry.io/otel v1.16.0
-	go.opentelemetry.io/otel/sdk v1.16.0
-	go.opentelemetry.io/otel/trace v1.16.0
+	go.opentelemetry.io/otel v1.18.0
+	go.opentelemetry.io/otel/sdk v1.18.0
+	go.opentelemetry.io/otel/trace v1.18.0
 )
 
 require (
-	cloud.google.com/go v0.110.6 // indirect
+	cloud.google.com/go v0.110.8 // indirect
 	cloud.google.com/go/compute v1.23.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/longrunning v0.5.1 // indirect
@@ -23,22 +23,22 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/s2a-go v0.1.4 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.2.5 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.1 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	go.opentelemetry.io/otel/metric v1.16.0 // indirect
-	golang.org/x/crypto v0.11.0 // indirect
-	golang.org/x/net v0.12.0 // indirect
-	golang.org/x/oauth2 v0.10.0 // indirect
+	go.opentelemetry.io/otel/metric v1.18.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/oauth2 v0.12.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.10.0 // indirect
-	golang.org/x/text v0.11.0 // indirect
-	google.golang.org/api v0.134.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20230726155614-23370e0ffb3e // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20230726155614-23370e0ffb3e // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230726155614-23370e0ffb3e // indirect
-	google.golang.org/grpc v1.57.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/api v0.143.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+	google.golang.org/grpc v1.58.2 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
