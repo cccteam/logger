c5d641d585f29bc3d7a115621ffb5c04160e02c9
jwatson-ccc <72507396+jwatson-ccc@users.noreply.github.com>
2023-11-06 18:08:54 -0500

    feature: Add request logging to ConsoleExporter (#11)
    * feature: Add request logging to ConsoleExporter
    
    * fix unit tests
    
    * Fix unit test
    
    * Add response size to log

--- console_test.go
+++ console_test.go
@@ -11,7 +11,10 @@ import (
 	"reflect"
 	"testing"
 
+	"cloud.google.com/go/logging"
 	"github.com/go-test/deep"
+	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 func TestNewConsoleExporter(t *testing.T) {
@@ -184,15 +187,16 @@ func TestNewConsoleLogger(t *testing.T) {
 				r:       &http.Request{},
 				noColor: true,
 			},
-			want: &consoleLogger{r: &http.Request{}, noColor: true},
+			want: &consoleLogger{r: &http.Request{}, noColor: true, maxSeverity: logging.Debug},
 		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			if got := newConsoleLogger(tt.args.r, tt.args.noColor); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewConsoleLogger() = %v, want %v", got, tt.want)
+			got := newConsoleLogger(tt.args.r, tt.args.noColor)
+			if diff := cmp.Diff(got, tt.want, cmp.AllowUnexported(consoleLogger{}), cmpopts.IgnoreFields(consoleLogger{}, "r", "mu")); diff != "" {
+				t.Errorf("NewConsoleLogger() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
@@ -218,17 +222,17 @@ func Test_consoleLogger(t *testing.T) {
 	}{
 		{
 			name: "Test with color", args: args{v: []any{"Message"}, v2: "Message"},
-			wantDebug: "\x1b[37mDEBUG\x1b[0m: /path Message\n", wantDebugf: "\x1b[37mDEBUG\x1b[0m: GET /path Formatted Message\n",
-			wantInfo: "\x1b[34mINFO \x1b[0m: /path Message\n", wantInfof: "\x1b[34mINFO \x1b[0m: GET /path Formatted Message\n",
-			wantWarn: "\x1b[33mWARN \x1b[0m: /path Message\n", wantWarnf: "\x1b[33mWARN \x1b[0m: GET /path Formatted Message\n",
-			wantError: "\x1b[31mERROR\x1b[0m: /path Message\n", wantErrorf: "\x1b[31mERROR\x1b[0m: GET /path Formatted Message\n",
+			wantDebug: "\x1b[37mDEBUG\x1b[0m: Message\n", wantDebugf: "\x1b[37mDEBUG\x1b[0m: Formatted Message\n",
+			wantInfo: "\x1b[34mINFO \x1b[0m: Message\n", wantInfof: "\x1b[34mINFO \x1b[0m: Formatted Message\n",
+			wantWarn: "\x1b[33mWARN \x1b[0m: Message\n", wantWarnf: "\x1b[33mWARN \x1b[0m: Formatted Message\n",
+			wantError: "\x1b[31mERROR\x1b[0m: Message\n", wantErrorf: "\x1b[31mERROR\x1b[0m: Formatted Message\n",
 		},
 		{
 			name: "Test no color", args: args{v: []any{"Message"}, v2: "Message", noColor: true},
-			wantDebug: "DEBUG: /path Message\n", wantDebugf: "DEBUG: GET /path Formatted Message\n",
-			wantInfo: "INFO : /path Message\n", wantInfof: "INFO : GET /path Formatted Message\n",
-			wantWarn: "WARN : /path Message\n", wantWarnf: "WARN : GET /path Formatted Message\n",
-			wantError: "ERROR: /path Message\n", wantErrorf: "ERROR: GET /path Formatted Message\n",
+			wantDebug: "DEBUG: Message\n", wantDebugf: "DEBUG: Formatted Message\n",
+			wantInfo: "INFO : Message\n", wantInfof: "INFO : Formatted Message\n",
+			wantWarn: "WARN : Message\n", wantWarnf: "WARN : Formatted Message\n",
+			wantError: "ERROR: Message\n", wantErrorf: "ERROR: Formatted Message\n",
 		},
 	}
 	for _, tt := range tests {
--- go.mod
+++ go.mod
@@ -5,40 +5,40 @@ go 1.21
 require (
 	cloud.google.com/go/logging v1.8.1
 	contrib.go.opencensus.io/exporter/stackdriver v0.13.14
-	github.com/go-playground/errors/v5 v5.3.3
+	github.com/go-playground/errors/v5 v5.4.0
 	github.com/go-test/deep v1.1.0
-	github.com/google/go-cmp v0.5.9
+	github.com/google/go-cmp v0.6.0
 	go.opentelemetry.io/otel v1.19.0
 	go.opentelemetry.io/otel/sdk v1.19.0
 	go.opentelemetry.io/otel/trace v1.19.0
 )
 
 require (
-	cloud.google.com/go v0.110.8 // indirect
-	cloud.google.com/go/compute v1.23.0 // indirect
+	cloud.google.com/go v0.110.10 // indirect
+	cloud.google.com/go/compute v1.23.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/longrunning v0.5.1 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
+	cloud.google.com/go/longrunning v0.5.4 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-playground/pkg/v5 v5.21.2 // indirect
+	github.com/go-playground/pkg/v5 v5.22.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.1 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/otel/metric v1.19.0 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
-	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/oauth2 v0.12.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
-	google.golang.org/api v0.143.0 // indirect
+	google.golang.org/api v0.149.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/genproto v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231002182017-d307bd883b97 // indirect
-	google.golang.org/grpc v1.58.2 // indirect
+	google.golang.org/genproto v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
